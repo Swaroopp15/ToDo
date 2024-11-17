f99fcd6d5474536eb059cb0cbc73ff08469222e2
Swaroopp15 <swarooppalacharla05@gmail.com>
2024-11-17 11:54:41 +0530

    Re initilized the entaire angular and git repo

---
+++ .angular/cache/18.2.12/project/vite/deps/browser-575MNK6W.js
@@ -0,0 +1,16 @@
+import {
+  Headers,
+  Request,
+  Response,
+  browser_default,
+  fetch
+} from "./chunk-UYZFHDIX.js";
+import "./chunk-3OV72XIM.js";
+export {
+  Headers,
+  Request,
+  Response,
+  browser_default as default,
+  fetch
+};
+//# sourceMappingURL=browser-575MNK6W.js.map
